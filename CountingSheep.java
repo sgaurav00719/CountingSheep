98f197696f24c6223e53a2b809f819963380696f
Gaurav Sharma <iNviNciBLe@Gauravs-MacBook-Pro.local>
2016-04-24 17:45:01 -0400

    first commit

--- CountingSheep.java
+++ CountingSheep.java
@@ -1,3 +1,36 @@
+/*
+Problem
+
+Bleatrix Trotter the sheep has devised a strategy that helps her fall asleep faster. First, she picks a number N. Then she starts naming N, 2 × N, 3 × N, and so on. Whenever she names a number, she thinks about all of the digits in that number. She keeps track of which digits (0, 1, 2, 3, 4, 5, 6, 7, 8, and 9) she has seen at least once so far as part of any number she has named. Once she has seen each of the ten digits at least once, she will fall asleep.
+
+Bleatrix must start with N and must always name (i + 1) × N directly after i × N. For example, suppose that Bleatrix picks N = 1692. She would count as follows:
+
+N = 1692. Now she has seen the digits 1, 2, 6, and 9.
+2N = 3384. Now she has seen the digits 1, 2, 3, 4, 6, 8, and 9.
+3N = 5076. Now she has seen all ten digits, and falls asleep.
+What is the last number that she will name before falling asleep? If she will count forever, print INSOMNIA instead.
+
+Input
+
+The first line of the input gives the number of test cases, T. T test cases follow. Each consists of one line with a single integer N, the number Bleatrix has chosen.
+
+Output
+
+For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is the last number that Bleatrix will name before falling asleep, according to the rules described in the statement.
+
+Limits
+
+1 ≤ T ≤ 100.
+Small dataset
+
+0 ≤ N ≤ 200.
+Large dataset
+
+0 ≤ N ≤ 106. 
+ 
+ */
+
+
 package Problem1;
 
 import java.io.FileReader;
@@ -18,8 +51,8 @@ public class CountingSheep {
 	}
 	
 	public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new FileReader("/Users/iNviNciBLe/Desktop/A-large.in"));
-        PrintWriter pw = new PrintWriter(new FileWriter("/Users/iNviNciBLe/Desktop/outputAlarge"));
+        Scanner sc = new Scanner(new FileReader("input.in"));
+        PrintWriter pw = new PrintWriter(new FileWriter("output.out"));
         int array[] = new int[10];
         int testCase = sc.nextInt();
         for ( int i = 0; i< testCase ; i++){
@@ -29,7 +62,6 @@ public class CountingSheep {
         	int count = 1;
         	int N = sc.nextInt();
         	if ( N == 0){
-        		System.out.println("Case #"+(i+1)+": INSOMNIA");
         		pw.println("Case #"+(i+1)+": INSOMNIA");
         	}
         	else
@@ -44,7 +76,6 @@ public class CountingSheep {
         			}
         			int checkSum = sumOfArray(array);
         			if (checkSum == 10){
-        				System.out.println("case #"+ (i+1) +": "+ numberAsString);
         				pw.println("case #"+ (i+1) +": "+ numberAsString);
         				break;
         			}
